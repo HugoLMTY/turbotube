f68dd4478b8fabf1099781b7c8eedcfd75beda00
HugoLMTY <hugo.lm2707@gmail.com>
2024-07-02 21:30:49 +0200

    Updates

---
+++ assets/axios.d1ad3420.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index.c0b99ffa.js";import{a}from"./axios.bf56c3c5.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
